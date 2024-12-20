08fdf0e63f2172808f97e388e2b088066f232401
ayushipaladiya246892 <ayushipaladiya92@gmail.com>
2024-12-21 01:38:02 +0530

    complete home page

--- Uber-Video/src/Pages/Home.jsx
+++ Uber-Video/src/Pages/Home.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div>
-      
+      {/* Example with a traffic light image */}
+      <div
+        className="bg-cover bg-center h-screen flex pt-8 justify-between flex-col w-full"
+        style={{ backgroundImage: 'url("assets/images/photo-1557404763-69708cd8b9ce.avif")' }}
+      >
+        <img
+          className="w-16 ml-8"
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+          alt=""
+        />
+        <div className="bg-white py-4 px-4 pb-7">
+          <h2 className="text-3xl font-bold">Get Started With Uber</h2>
+          <Link to='/login' className="flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5">Continue</Link>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- Uber-Video/src/Pages/UserLogin.jsx
+++ Uber-Video/src/Pages/UserLogin.jsx
@@ -2,8 +2,35 @@ import React from 'react'
 
 function UserLogin() {
   return (
-    <div>
-      
+    <div className='p-7'>
+        <div>
+        <img
+          className="w-16 mb-10"
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+          alt=""
+        />
+        <form>
+        <h3 className='text-lg font-medium mb-2 '>What's your email</h3>
+        <input 
+        type='email' 
+        className='bg-[#eeeeee] mb-7  border rounded px-4 w-full text-lg py-2 placeholder:text-base'
+        required 
+        placeholder='email@example.com'/>
+
+        <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
+
+        <input 
+        required 
+        className='bg-[#eeeeee] mb-7  border rounded px-4 w-full text-lg py-2 placeholder:text-base'
+        type='password'
+        placeholder='password'/>
+
+        <button className='bg-[#111] mb-7 rounded px-4 w-full text-lg py-2 text-white font-semibold '>Login</button>
+        </form>
+        </div>
+        <div>
+            
+        </div>
     </div>
   )
 }
