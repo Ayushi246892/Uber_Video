3abb89140473657f9ccec32387bed76e9b29441c
ayushipaladiya246892 <ayushipaladiya92@gmail.com>
2024-12-27 21:26:50 +0530

    app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,55 @@
 import React, { useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import UserLogin from './Pages/UserLogin'
-import UserSignup from './Pages/UserSignup'
-import CaptainLogin from './Pages/CaptainLogin'
-import CaptainSignup from './Pages/CaptainSignup'
-import { UserDataContext } from './Context/Usercontext'
-import Start from './Pages/Start'
-import UserHome from './Pages/UserHome'
-import Locationsearchpanel from './component/Locationsearchpanel'
-import ConfirmRide from "./component/ConfirmRide";
-import Vehiclepanel from "./component/Vehiclepanel";
-import WaitingforDriver from './component/WaitingforDriver'
-import LookingforDriver from './component/LookingforDriver'
-import Riding from './Pages/Riding'
-import Captainhome from './Pages/Captainhome'
-import Captaindetails from './component/Captaindetails'
-import Ridepopup from './component/Ridepopup'
-import Confirmridepopup from './component/Confirmridepopup'
-import Captainriding from './Pages/Captainriding'
-import Finishride from './component/Finishride'
+import Start from './pages/Start'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import Captainlogin from './pages/Captainlogin'
+import CaptainSignup from './pages/CaptainSignup'
+import Home from './pages/Home'
+import UserProtectWrapper from './pages/UserProtectWrapper'
+import UserLogout from './pages/UserLogout'
+import CaptainHome from './pages/CaptainHome'
+import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
+import CaptainLogout from './pages/CaptainLogout'
+import Riding from './pages/Riding'
+import CaptainRiding from './pages/CaptainRiding'
+import 'remixicon/fonts/remixicon.css'
 
-function App  () {
+const App = () => {
 
-  
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Start/>}/>
-        <Route path='/home' element={<UserHome/>}/>
-        <Route path='/login' element={<UserLogin/>}/>
-        <Route path='/signup' element={<UserSignup/>}/>
-        <Route path='/captain-login' element={<CaptainLogin/>}/>
-        <Route path='/captain-signup' element={<CaptainSignup/>}/>
-        <Route path='/locationsearch-panel' element={<Locationsearchpanel/>}/>
-        <Route path='/confirmride' element={<ConfirmRide/>}/>
-        <Route path='/vehiclepanel' element={<Vehiclepanel/>}/>
-        <Route path='/waitfordriver' element={<WaitingforDriver/>}/>
-        <Route path='/lookingfordriver' element={<LookingforDriver/>}/>
-        <Route path='/riding' element={<Riding/>}/>
-        <Route path='/captainhome' element={<Captainhome/>}/>
-        <Route path='/captaindetails' element={<Captaindetails/>}/>
-        <Route path='/ridepopup' element={<Ridepopup/>}/>
-        <Route path='/confirmridepopup' element={<Confirmridepopup/>}/>
-        <Route path='/captainriding' element={<Captainriding/>}/>
-        <Route path='/finishride' element={<Finishride/>}/>
+        <Route path='/' element={<Start />} />
+        <Route path='/login' element={<UserLogin />} />
+        <Route path='/riding' element={<Riding />} />
+        <Route path='/captain-riding' element={<CaptainRiding />} />
+
+        <Route path='/signup' element={<UserSignup />} />
+        <Route path='/captain-login' element={<Captainlogin />} />
+        <Route path='/captain-signup' element={<CaptainSignup />} />
+        <Route path='/home'
+          element={
+            <UserProtectWrapper>
+              <Home />
+            </UserProtectWrapper>
+          } />
+        <Route path='/user/logout'
+          element={<UserProtectWrapper>
+            <UserLogout />
+          </UserProtectWrapper>
+          } />
+        <Route path='/captain-home' element={
+          <CaptainProtectWrapper>
+            <CaptainHome />
+          </CaptainProtectWrapper>
+
+        } />
+        <Route path='/captain/logout' element={
+          <CaptainProtectWrapper>
+            <CaptainLogout />
+          </CaptainProtectWrapper>
+        } />
       </Routes>
     </div>
   )
